9c8ded39afb4f85793fbfae2df22f8ba196f43a4
suohyd <86884865+suohyd@users.noreply.github.com>
2025-10-21 15:47:23 +0530

    ---

--- output/web/lunr-pretext-search-index.js
+++ output/web/lunr-pretext-search-index.js
@@ -1618,7 +1618,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet",
   "number": "3.7",
   "title": "Exercises related to Artinian and Noetherian modules",
-  "body": " Exercises related to Artinian and Noetherian modules   Most of the exercises are taken from and . Throughout this worsheet we assume that is a commutative ring with unity and that is a right -algebra. We assume that is a right -module.     Show that if is Noetherian (resp., Artinian) then, every finitely-generated right -module is Noetherian (resp., Artinian). In particular, show that if is a division ring and is a finitely-generated right -module, then is Noetherian and Artinian.      Show that if an endomorphism of a right Noetherian (resp., Artinian) module is epimorphism (resp., monomorphism), then it is an isomorphism.      Let be a vector space over a field with a countable basis . Suppose that is an -linear endomorphism of . Using we consider as a right -module.   Let be defined by and for . Show that -module is Artinian.    Let be defined by for . Show that -module is Noetherian.         Let be a right -module of finite length , and be submodules of . Show the following.     Use the isomorphism and the following short exact sequences.      "
+  "body": " Exercises related to Artinian and Noetherian modules   Most of the exercises are taken from and . Throughout this worsheet we assume that is a commutative ring with unity and that is a right -algebra. We assume that is a right -module.     Show that if is Noetherian (resp., Artinian) then, every finitely-generated right -module is Noetherian (resp., Artinian). In particular, show that if is a division ring and is a finitely-generated right -module, then is Noetherian and Artinian.      Show that if an endomorphism of a right Noetherian (resp., Artinian) module is epimorphism (resp., monomorphism), then it is an isomorphism.      Let be a vector space over a field with a countable basis . Suppose that is an -linear endomorphism of . Using we consider as a right -module.   Let be defined by and for . Show that -module is Artinian.    Let be defined by for . Show that -module is Noetherian.         Let be a right -module of finite length , and be submodules of . Show the following.     Use the isomorphism and the following short exact sequences.        Determine the length of the -module for a prime integer and a positive integer .    "
 },
 {
   "id": "ws-artinian-noetherian-3",
@@ -1655,6 +1655,15 @@ var ptx_lunr_docs = [
   "number": "3.7.4",
   "title": "",
   "body": "  Let be a right -module of finite length , and be submodules of . Show the following.     Use the isomorphism and the following short exact sequences.     "
+},
+{
+  "id": "ws-artinian-noetherian-7",
+  "level": "2",
+  "url": "ws-artinian-noetherian.html#ws-artinian-noetherian-7",
+  "type": "Worksheet Exercise",
+  "number": "3.7.5",
+  "title": "",
+  "body": "  Determine the length of the -module for a prime integer and a positive integer .   "
 },
 {
   "id": "sec-radical",
